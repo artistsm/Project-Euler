e2bbfcbb7db7e28c2c71701e93748df87a3c63dd
artistsm <shilpionweb@gmail.com>
2020-10-02 19:27:52 -0500

    Prob 4 solved

--- prob4.py
+++ prob4.py
@@ -1,31 +1,30 @@
-n1 = 999
-count = 0
-
 flag = False
+
+def is_palindrome(N):
+    N_str = str(N) #Converting the number to a string
+    mid = int(len(N_str)/2)
+    if(N_str[0:mid]==N_str[len(N_str)-1:mid-1:-1]):
+        return True
+    return False
+
 def palindrome():
-    for i in reversed(range(999)):
-        for j in reversed(range(999)):
-            mul = i*j
-            mul_str = str(mul)
-            if len(mul_str) == 4 and mul_str[0] == mul_str[3] and mul_str[1] == mul_str[2]:
-                print(mul_str)
-                flag = True
-                break
-            elif len(mul_str) == 5 and  mul_str[0] == mul_str[4] and mul_str[1] == mul_str[3]:
-                print(mul_str)
-                flag = True
-                break
-            elif len(mul_str) == 6 and mul_str[0] == mul_str[5] and mul_str[1] == mul_str[4] and mul_str[2] == mul_str[3]:
-                print(mul_str)
-                flag = True
-                break
+    for i in range(999*999, 100*100, -1): 
+        flag = False           
+        if is_palindrome(i) == True:
+            for j in range(999,99,-1):
+                if (i %j == 0) and (len(str(int(i/j))) == 3):
+                    print(i) 
+                    flag = True 
+                    break  
         if flag == True:
             break
+        
 
 
 if __name__ == "__main__":
-    print("Project Euler Problem 1")
-    palindrome()            
+    print("Project Euler Problem 4")
+    palindrome()   
+    # print(is_palindrome(989989)         )
 
 
 
